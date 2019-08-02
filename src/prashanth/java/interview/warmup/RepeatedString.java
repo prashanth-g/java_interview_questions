ba07c6a57eec9defcf1fb3a4869f10001dd47e9a
prashanth-g <prashanthcsgce@gmail.com>
2019-08-02 22:07:18 +0530

    Added RepeatedString class

---
+++ src/prashanth/java/interview/warmup/RepeatedString.java
@@ -0,0 +1,26 @@
+package prashanth.java.interview.warmup;
+
+public class RepeatedString {
+
+  public static void main(String[] args) {
+    long n = Long.parseLong("1000000000000");
+    System.out.println(repeatedString("a", n));
+  }
+
+  static long repeatedString(String s, long n) {
+    int charCount = getCharCount(s);
+    long numberOfTimes = n / s.length();
+    String remaingString = s.substring(0, (int) (n - numberOfTimes * s.length()));
+    return numberOfTimes * charCount + getCharCount(remaingString);
+  }
+
+  private static int getCharCount(String s) {
+    int charCount = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if(s.charAt(i) == 'a') {
+        charCount++;
+      }
+    }
+    return charCount;
+  }
+}
