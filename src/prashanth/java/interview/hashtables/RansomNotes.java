41a58972f832f016572bed46c16a2436999a1092
prashanth-g <prashanthcsgce@gmail.com>
2019-08-05 00:01:01 +0530

    Added RansomNotes

---
+++ src/prashanth/java/interview/hashtables/RansomNotes.java
@@ -0,0 +1,49 @@
+package prashanth.java.interview.hashtables;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class RansomNotes {
+
+  public static void main(String[] args) {
+    String[] magazine = "give me one grand today night".split(" ");
+    String[] notes = "give one grand today".split(" ");
+    checkMagazine(magazine, notes);
+  }
+
+  static void checkMagazine(String[] magazine, String[] note) {
+    // Set words and its count
+    Hashtable<String, Integer> wordsCount = new Hashtable<>();
+    for (String word : magazine) {
+      if (wordsCount.get(word) == null) {
+        wordsCount.put(word, 1);
+      } else {
+        wordsCount.put(word, wordsCount.get(word) + 1);
+      }
+    }
+
+    Hashtable<String, Integer> notesCount = new Hashtable<>();
+    for (String word : note) {
+      if (notesCount.get(word) == null) {
+        notesCount.put(word, 1);
+      } else {
+        notesCount.put(word, notesCount.get(word) + 1);
+      }
+    }
+
+    Enumeration<String> enumeration = notesCount.keys();
+    while (enumeration.hasMoreElements()) {
+      String key = enumeration.nextElement();
+      if (wordsCount.get(key) == null) {
+        System.out.println("No");
+        return;
+      } else {
+        if (notesCount.get(key) > wordsCount.get(key)) {
+          System.out.println("No");
+          return;
+        }
+      }
+    }
+    System.out.println("Yes");
+  }
+}
