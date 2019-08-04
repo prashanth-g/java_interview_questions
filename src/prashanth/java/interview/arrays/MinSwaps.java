b22123e641d43f895685c910b68905b7200e3bb6
prashanth-g <prashanthcsgce@gmail.com>
2019-08-04 08:25:39 +0530

    Added MinSwaps

---
+++ src/prashanth/java/interview/arrays/MinSwaps.java
@@ -0,0 +1,25 @@
+package prashanth.java.interview.arrays;
+
+public class MinSwaps {
+
+  public static void main(String[] args) {
+    int[] arr = {2, 1, 5, 3, 4};
+    System.out.println(minimumSwaps(arr));
+  }
+
+  static int minimumSwaps(int[] arr) {
+    int swapCount = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == i + 1) { // no swap
+        continue;
+      } else {
+        int temp = arr[i];
+        arr[i] = arr[temp - 1];
+        arr[temp - 1] = temp;
+        swapCount++;
+        i--;
+      }
+    }
+    return swapCount;
+  }
+}
